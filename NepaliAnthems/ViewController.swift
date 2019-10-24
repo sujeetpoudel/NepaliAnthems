8a2c7742dd8154895ccfc2d4d9202808cc03a83a
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-10-24 11:45:44 -0500

    Initial Commit

---
+++ NepaliAnthems/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NepaliAnthems
+//
+//  Created by IMCS on 10/24/19.
+//  Copyright © 2019 IMCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
