4013af4b866859101eeccc0365ec1a8f28d27ccc
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-11-01 23:45:51 -0500

    First

---
+++ NepaliAnthems/PlayViewController.swift
@@ -0,0 +1,145 @@
+//
+//  PlayViewController.swift
+//  NepaliAnthems
+//
+//  Created by IMCS on 10/24/19.
+//  Copyright © 2019 IMCS. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayViewController: UIViewController {
+    
+    var songName : String = ""
+    var player = AVPlayer()
+    
+    var isPlaying : Bool = true
+    
+    @IBOutlet weak var TitleLabel: UILabel!
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var playPauseButton: UIButton!
+    
+    @IBOutlet weak var TimeLabel: UILabel!
+    
+    @IBOutlet weak var audioSlider: UISlider!
+    
+    @IBAction func audioSliderChange(_ sender: UISlider) {
+        print(audioSlider.value)
+        if let duration = player.currentItem?.duration{
+            let totalSeconds = Float(CMTimeGetSeconds(duration))
+            let value = audioSlider.value *  totalSeconds
+            let seekTime = CMTime(value: CMTimeValue(value), timescale: 1)
+            player.seek(to: seekTime, completionHandler : {(completedSeek)
+                in
+                
+            })
+        }
+    }
+    
+    
+    @IBAction func playPauseButtonAction(_ sender: UIButton) {
+        
+        if isPlaying {
+            isPlaying = false
+            player.pause()
+            playPauseButton.setImage(UIImage(named: "play.png"), for: .normal)
+        } else {
+            isPlaying = true
+            player.play()
+            playPauseButton.setImage(UIImage(named: "pause.png"), for: .normal)
+        }
+        
+        
+    }
+
+    @objc func back(sender: UIBarButtonItem) {
+        // Perform your custom actions
+        // ...
+        // Go back to the previous ViewController
+        _ = navigationController?.popViewController(animated: true)
+        player.pause()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        playSong()
+        TitleLabel.text = songName
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        self.navigationItem.hidesBackButton = true
+        let newBackButton = UIBarButtonItem(title: "Back", style: UIBarButtonItem.Style.plain, target: self, action: #selector(PlayViewController.back(sender:)))
+        self.navigationItem.leftBarButtonItem = newBackButton
+        
+        let interval = CMTimeMake(value: 1, timescale: 4) // 0.25 (1/4) seconds
+       
+        player.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main) { [weak self] time in
+            //print(time)
+            
+            let time = Int(CMTimeGetSeconds((self?.player.currentTime())!)) 
+            
+            if let totalTime = self?.player.currentItem?.duration {
+                
+           
+                
+            let totalSeconds = Float(CMTimeGetSeconds(totalTime))
+                guard !(totalSeconds.isNaN || totalSeconds.isInfinite) else {
+                               return
+                }
+                print(totalSeconds)
+                print(Float(time)/totalSeconds)
+                self?.audioSlider.value = Float(time)/Float(totalSeconds)
+            }
+            
+            let minutes = Int(time) / 60
+            let seconds = Int(time) % 60
+            
+            self?.TimeLabel.text = String(format:"%02i:%02i", minutes, seconds)
+            
+            print(time)
+//            NSUInteger durationSeconds = (long)CMTimeGetSeconds(interval);
+//            NSUInteger hours = floor(dTotalSeconds / 3600);
+//            NSUInteger minutes = floor(durationSeconds % 3600 / 60);
+//            NSUInteger seconds = floor(durationSeconds % 3600 % 60);
+        }
+       
+        // Do any additional setup after loading the view.
+    }
+    
+    func playSong() {
+        var audioPath = Bundle.main.path(forResource: songName, ofType: "mp3")
+        
+        
+        do {
+            player = try AVPlayer (url: URL(fileURLWithPath: audioPath!))
+                //NameofSong.text = songsName[currentSong]
+        } catch {
+            print("Error Page")
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+        }
+        catch {
+           // report for an error
+        }
+        
+        player.play()
+    }
+    
+    @IBAction func volumeSlider(_ sender: Any) {
+        player.volume = volumeSlider.value
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NepaliAnthems/ViewController.swift
+++ NepaliAnthems/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var NationalSongsButton: UIButton!
+    
+    @IBOutlet weak var ShareButton: UIButton!
+    
+    
+    @IBOutlet weak var RateButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        styleFilledButton(NationalSongsButton)
+        styleFilledButton(ShareButton)
+        styleFilledButton(RateButton)
+        
+    }
+    
+    @IBAction func ShareAction(_ sender: UIButton) {
+        
+        let activity = UIActivityViewController(
+                   activityItems: ["Here is the link of this app : ..."],
+                   applicationActivities: nil
+               )
+               present(activity, animated: true, completion: nil)
+    }
+    func styleFilledButton(_ button:UIButton) {
+        
+        // Filled rounded corner style
+       button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 15.0
+        button.tintColor = UIColor.blue
+        button.titleLabel?.textColor = UIColor.blue
     }
 
 
